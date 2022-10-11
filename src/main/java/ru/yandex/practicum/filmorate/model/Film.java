6050e263515b485d77f237dd1fd2a8d4fe8472f9
Max.Terekhov <terekhovmv@yandex.ru>
2022-10-11 17:55:17 +0300

    feat: adds validating annotations to models

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,15 +2,24 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
 import lombok.Data;
+import ru.yandex.practicum.filmorate.annotations.MovieEra;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
 @Builder(toBuilder = true)
 public class Film {
     private int id;
+    @NotBlank
     private String name;
+    @Size(min = 0, max = 200)
     private String description;
+    @MovieEra
     private LocalDate releaseDate;
+    @Positive
     private int duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,14 +3,20 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 @Data
 @Builder(toBuilder = true)
 public class User {
     private int id;
+    @Email
     private String email;
+    @NotBlank
     private String login;
     private String name;
+    @Past
     private LocalDate birthday;
 }
